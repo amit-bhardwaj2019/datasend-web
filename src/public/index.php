3702f7e8a892f7850751ac1e64087bedb6e66e38
amit-bhardwaj2019 <amit.bhardwaj@nethues.com>
2023-02-07 16:23:32 +0530

    add header to request

--- src/public/index.php
+++ src/public/index.php
@@ -38,8 +38,8 @@ $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
             ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8888')
-            ->withHeader('Access-Control-Allow-Credentials: true
-            ')
+            ->withHeader('Access-Control-Allow-Credentials', true
+            )
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
