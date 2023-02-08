e3dcdae25b79b833f0a089e40205e76e712cbf80
amit-bhardwaj2019 <amit.bhardwaj@nethues.com>
2023-02-08 12:13:10 +0530

    update

--- src/public/index.php
+++ src/public/index.php
@@ -40,6 +40,7 @@ $app->add(function ($req, $res, $next) {
             ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8888')
             ->withHeader('Access-Control-Allow-Credentials', true
             )
+            ->withAddedHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
