bae372f3b9f9869836721e874ed07b8e11e70491
amit-bhardwaj2019 <amit.bhardwaj@nethues.com>
2023-02-24 13:53:47 +0530

    update

--- src/app/Controller/AdminUserController.php
+++ src/app/Controller/AdminUserController.php
@@ -273,7 +273,7 @@ class AdminUserController {
                     $input_data['token'] = $token;
                     $result = (int)$this->adminGateway->insertMainUser($input_data);
                     // var_export($this->ci->get('common')->SendSubUserLoginEmail($result));
-                    var_export($result);
+                    var_export($result);die('hello');
                     if(is_int($result)) {
                         $this->returnData['message'] = 'User added successfully.';
                         $r =  json_encode($this->returnData);
